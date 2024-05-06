46ee0c8c6651906b458c4826d959e16df7de703f
Mark Kimathi <mark.kimathi@technobraingroup.com>
2024-05-06 11:15:31 +0300

    home pg

--- src/App.js
+++ src/App.js
@@ -1,25 +1,12 @@
-import logo from './logo.svg';
-import './App.css';
-import Navbar from './components/Navbar';
-
+ import './App.css';
+import DefaultLayout from './layout/DefaultLayout';
+import Home from './pages/clergy/Home';
+ 
 function App() {
   return (
     <div className="App">
-      <Navbar/>
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <DefaultLayout/> 
+      <Home/>
     </div>
   );
 }
---
+++ src/pages/clergy/Home.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import './Home.css'
+const Home = () => {
+  return (
+    <header className="main-page">
+         
+       sdrfews
+         
+    </header>
+  )
+}
+
+export default Home
