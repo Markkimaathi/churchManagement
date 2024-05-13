5637fab5a90b860f73ff5e5b82e38268385bd390
Mark Kimathi <mark.kimathi@technobraingroup.com>
2024-05-13 08:19:42 +0300

    modules

---
+++ src/components/PrayerRequests.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const PrayerRequests = () => {
+  return (
+    <div>
+      requests to be submitted by the members, but can only be viewed by the clergy
+    </div>
+  )
+}
+
+export default PrayerRequests
