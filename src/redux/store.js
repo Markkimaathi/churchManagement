007e7898b0d58735c8ec2031a1270028da669c6c
Mark Kimathi <mark.kimathi@technobraingroup.com>
2024-05-27 09:46:58 +0300

    req stored

--- src/redux/store.js
+++ src/redux/store.js
@@ -2,11 +2,13 @@
 import { configureStore } from '@reduxjs/toolkit';
 import getUsersReducer from './reducers/GetUsersReducer';
 import getAnnouncementsReducer from './reducers/GetAnnouncementsReducer';
+import GetPrayerRequestsReducer from './reducers/GetPrayerRequestsReducer';
 
 const store = configureStore({
   reducer: {
     Users: getUsersReducer,
-    Announcements: getAnnouncementsReducer
+    Announcements: getAnnouncementsReducer,
+    PrayerRequests: GetPrayerRequestsReducer
   }
 });
 
