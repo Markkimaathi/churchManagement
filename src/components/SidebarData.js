6faed1e45bde6909d572c3e593b9b6e82d58bffa
Mark Kimathi <mark.kimathi@technobraingroup.com>
2024-05-28 14:44:58 +0300

    admin undefined

--- src/components/SidebarData.js
+++ src/components/SidebarData.js
@@ -2,7 +2,6 @@ import { BsGrid1X2Fill, BsChatDotsFill, BsMenuButtonWideFill,} from 'react-icons
 import { IoPerson } from 'react-icons/io5';
 import { RiCalendar2Fill } from 'react-icons/ri';
 import EventsCalendar from './EventsCalendar/EventsCalendar.js';
-import { FaUsers } from "react-icons/fa";
 import { FaBell } from "react-icons/fa";
 import { FaTable } from "react-icons/fa";
 import UserTable from '../pages/Admin/UserTable/UserTable.js';
@@ -22,11 +21,6 @@ export const adminSidebarData = [
     icon: FaTable,
     link:'/all-users',
     component: UserTable
-  },
-  {
-    title: 'Admin Profile Management',
-    icon: FaUsers,
-    link: '/Admin-Profile-Management'
   },
   {
     title: 'Announcements',
