b2e69302023443e865c3c7935ffb19f5beb2d367
Mark Kimathi <mark.kimathi@technobraingroup.com>
2024-06-05 09:01:45 +0300

    update announcement action

---
+++ src/redux/actions/UpdateAnnouncementAction.js
@@ -0,0 +1,31 @@
+import { createAsyncThunk } from "@reduxjs/toolkit";
+
+
+  //Update
+  export const UpdateAnnouncementDetails = createAsyncThunk('fetch/UpdateUserDetails', async ({myForm, id}) => {
+    try {
+      const API_ENDPOINT_URL = `http://localhost:81/api/Announcement/Update/${id}`;
+      const response = await fetch(API_ENDPOINT_URL, {
+        method: 'PUT',
+        headers: {
+          'Content-Type': 'application/json',
+        },       
+        body: JSON.stringify(myForm)
+      });  
+  
+      if (response.ok) {
+        const data = await response.text();
+        return data; 
+        
+      } else {
+        const data = await response.text();
+        return data; 
+      }
+    } catch (error) {
+      console.error('Error during the PUT request:', error);
+      throw new Error(`Error during the PUT request: ${error.message}`);
+    }
+  });
+
+
+    
