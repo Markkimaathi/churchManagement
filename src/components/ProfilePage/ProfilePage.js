9b31ec2f2ea3374eefbe7acb3daae870419ab8bb
Mark Kimathi <mark.kimathi@technobraingroup.com>
2024-06-07 09:08:19 +0300

    events table

--- src/components/ProfilePage/ProfilePage.js
+++ src/components/ProfilePage/ProfilePage.js
@@ -1,7 +1,6 @@
 import React, { useState, useEffect } from "react";
 import './ProfilePage.css'; 
 
-
 const ProfilePage = () => {
   const [FullName, setFullName] = useState("");
   const [dateOfBirth, setDateOfBirth] = useState("");
