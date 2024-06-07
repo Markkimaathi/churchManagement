4f41dcd8b6196ddd41e9e78b8be4066706687d18
Mark Kimathi <mark.kimathi@technobraingroup.com>
2024-06-07 09:41:40 +0300

    del events action

---
+++ src/redux/actions/DeleteEventsAction.js
@@ -0,0 +1,25 @@
+import { createAsyncThunk } from "@reduxjs/toolkit";
+
+export const DeleteEvents = createAsyncThunk( 'fetch/DeleteEvents', async ({ myForm, id }, { rejectWithValue }) => {
+    try {
+      const API_ENDPOINT_URL = `http://localhost:81/api/Events/Delete/${id}`;
+
+      const response = await fetch(API_ENDPOINT_URL, {
+        method: 'DELETE',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(myForm)
+      });  
+      if (response.ok) {
+        const data = await response.text();
+        return data;      
+      } else {
+        const data = await response.text();
+        return data; 
+      }
+    } catch (error) {
+      console.error('Error during the DELETE request:', error);
+      throw new Error(`Error during the DELETE request: ${error.message}`);
+    }
+  });
