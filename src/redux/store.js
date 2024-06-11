740fe29c086e2f3cfcdc0112aae8235d8612fdcc
Mark Kimathi <mark.kimathi@technobraingroup.com>
2024-06-11 11:29:22 +0300

    admins stored

--- src/redux/store.js
+++ src/redux/store.js
@@ -5,6 +5,7 @@ import GetPrayerRequestsReducer from './reducers/GetPrayerRequestsReducer';
 import GetEventsReducer from './reducers/GetEventsReducer';
 import GetMembersReducer from './reducers/GetMembersReducer';
 import GetClergiesReducer from './reducers/GetClergiesReducer';
+import GetAdminsAction from './reducers/GetAdminsAction';
 
 const store = configureStore({
   reducer: {
@@ -13,7 +14,8 @@ const store = configureStore({
     PrayerRequests: GetPrayerRequestsReducer,
     Events: GetEventsReducer,
     Members: GetMembersReducer,
-    Clergies: GetClergiesReducer
+    Clergies: GetClergiesReducer,
+    Admins: GetAdminsAction
   }
 });
 
