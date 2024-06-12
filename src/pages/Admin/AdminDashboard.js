23c9f441a2a29ca85358fc6508ba0db796c20634
Mark Kimathi <mark.kimathi@technobraingroup.com>
2024-06-12 11:49:32 +0300

    admin

--- src/pages/Admin/AdminDashboard.js
+++ src/pages/Admin/AdminDashboard.js
@@ -4,50 +4,39 @@ import { GetAllUsers } from '../../redux/actions/LoginAction';
 import { GetAllAdmins } from '../../redux/actions/AdminsAction';
 import { GetAllClergies } from '../../redux/actions/ClergyAction';
 import { GetAllMembers } from '../../redux/actions/MembersAction';
+import { GetAllEvents } from '../../redux/actions/EventsAction';
+import { GetAllAnnouncements } from '../../redux/actions/AnnouncementsAction';
 import { Button } from '@mui/material';
 import MetaData from '../../components/MetaData';
-import { BsFillArchiveFill, BsFillGrid3X3GapFill, BsPeopleFill, BsPersonFill } from 'react-icons/bs';
+import { BsFillArchiveFill, BsFillGrid3X3GapFill, BsPeopleFill } from 'react-icons/bs';
 import { Link } from 'react-router-dom';
 import './AdminDashboard.css';
+import { FaBell } from "react-icons/fa";
 
 const AdminDashboard = () => {
   const { allUsers } = useSelector((state) => state.Users);
   const { allMembers } = useSelector((state) => state.Members);
   const { allClergies } = useSelector((state) => state.Clergies);
   const { allAdmins } = useSelector((state) => state.Admins);
+  const { allEvents } = useSelector((state) => state.Events);
+  const { allAnnouncements} = useSelector((state) => state.Announcements);
   const dispatch = useDispatch();
 
   useEffect(() => {
     dispatch(GetAllUsers());
-  }, [dispatch]);
-
-  useEffect(() => {
     dispatch(GetAllMembers());
-  }, [dispatch]);
-
-  const getMembersCount = () => {
-    return allMembers ? allMembers.length : 0;
-  };
-
-  useEffect(() => {
     dispatch(GetAllClergies());
-  }, [dispatch]);
-
-  const getClergiesCount = () => {
-    return allClergies ? allClergies.length : 0;
-  };
-
-  useEffect(() => {
     dispatch(GetAllAdmins());
+    dispatch(GetAllEvents());
+    dispatch(GetAllAnnouncements());
   }, [dispatch]);
 
-  const getAdminsCount = () => {
-    return allAdmins ? allAdmins.length : 0;
-  };
-
-  const getUsersCount = () => {
-    return allUsers ? allUsers.length : 0;
-  };
+  const getMembersCount = () => allMembers ? allMembers.length : 0;
+  const getClergiesCount = () => allClergies ? allClergies.length : 0;
+  const getAdminsCount = () => allAdmins ? allAdmins.length : 0;
+  const getUsersCount = () => allUsers ? allUsers.length : 0;
+  const getEventsCount = () => allEvents ? allEvents.length : 0;
+  const getAnnouncementsCount = () =>allAnnouncements ? allAnnouncements.length : 0;
 
   return (
     <main className='main-container'>
@@ -95,6 +84,26 @@ const AdminDashboard = () => {
           <h1>{getUsersCount()}</h1>
           <Link to="/all-users">
             <Button variant="contained" color="primary">Users</Button>
+          </Link>
+        </div>
+        <div className='card'>
+          <div className='card-inner'>
+            <h3>Events</h3>
+            <BsFillGrid3X3GapFill className='card_icon'/>
+          </div>
+          <h1>{getEventsCount()}</h1>
+          <Link to="/events">
+            <Button variant="contained" color="primary">Events</Button>
+          </Link>
+        </div>
+        <div className='card'>
+          <div className='card-inner'>
+            <h3>Announcements</h3>
+            <FaBell className='card_icon'/>
+          </div>
+          <h1>{getAnnouncementsCount()}</h1>
+          <Link to="/all-announcements">
+            <Button variant="contained" color="primary">Announcements</Button>
           </Link>
         </div>
       </div>
--- src/pages/Admin/AllAnnouncements/AllAnnouncementsTable.js
+++ src/pages/Admin/AllAnnouncements/AllAnnouncementsTable.js
@@ -13,6 +13,9 @@ import { GetAllAnnouncements } from '../../../redux/actions/AnnouncementsAction'
 import LoaderComponent from '../../../components/Loader/LoaderComponent';
 import MetaData from '../../../components/MetaData';
 import './AllAnnouncementsTable.css'
+import { Link } from 'react-router-dom';
+import { Button } from '@mui/material';
+
 
 export const AllAnnouncementsTable = () => {
   const { allAnnouncements, error, loading } = useSelector((state) => state.Announcements);
@@ -66,6 +69,9 @@ export const AllAnnouncementsTable = () => {
               ))}
             </TableBody>
           </Table>
+          <Link to="/dashboard">
+            <Button variant="contained" color="primary">Dashboard</Button>
+          </Link>
         </TableContainer>
       )}
     </div>
